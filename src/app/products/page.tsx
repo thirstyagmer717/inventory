94857f7d947331d3ebfff4e3f79fa093498bb348
Yash Chaudhary <thirstygamer717@gmail.com>
2023-08-31 03:10:08 +0530

    implement search using client

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,27 +1,40 @@
 import getProducts from "@/libs/getProducts";
+let list = new Array();
+let products = new Array();
+async function search(data:FormData){
+'use server'
+let search = await JSON.stringify(data.get('search') || "");
+search = search.substring(1,search.length-1); 
+console.log(search);
+const mySearch = search.split(',');
+console.log(mySearch);
+list.shift();
+}
+
 export default async function Products() {
-    let products = await getProducts();
-    
+    'use client'
+    products = await getProducts();
+    list = products;
     return (
         <>
         <div className="flex flex-col items-center mt-10">
+            <form className="flex gap-8" action={search}>
+            <input name="search" placeholder="Product Name" type="text" className=" border-2 border-black px-2 py-1 rounded"></input>
+            <button type="submit" className="bg-blue-200 px-4 py-1 rounded">Search</button>
+            </form>
                 <div className="flex justify-between ml-2 w-1/3 mt-4">
                     <span>name</span>{" "}
                     <span>vendor</span>{" "}
                     <span>qty_available</span>
                 </div>
-            {products.map((p)=>(
+            {list.map((p)=>(
                 <div key={p.id} className="flex justify-between w-1/3 mt-4">
                     <span>{p.name}</span>{" "}
                     <span>{p.vendor}</span>{" "}
                     <span>{p.qty_available}</span>
                 </div>
             ))}
-        </div>
-    
-    
-    
-    
+        </div>   
         </>
     )
 }
